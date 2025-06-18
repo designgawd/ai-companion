d43c58af87f9c41cf3ace97b13774e27bbcdb44b
Eric Dumas <ericdumas1980@gmail.com>
2025-06-17 19:41:49 -0700

    deployment test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,9 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  rules: {
+    "@typescript-eslint/no-unused-expression": "off"
+  }
 });
 
 const eslintConfig = [
